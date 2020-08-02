5d451f5a9d7ddac516b7fdf87ce340d16baa85c7
MoeinGhbh <39806570+MoeinGhbh@users.noreply.github.com>
2020-08-02 19:33:18 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////c//Users//Rose//Desktop//Flask Python//Restful API Python
-# Flask//database.db"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///c:\\Users\\Rose\\Desktop\\Flask Python\\Restful API Python " \
                                         "Flask\\database.db "
 
